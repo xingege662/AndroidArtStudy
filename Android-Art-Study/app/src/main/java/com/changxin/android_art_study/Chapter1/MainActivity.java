22c2a31a5a82c647a0ac66bf23b642831971408a
xingege662 <924717778@qq.com>
2016-12-28 11:37:28 +0800

    34页

--- Android-Art-Study/app/src/main/java/com/changxin/android_art_study/Chapter1/MainActivity.java
+++ Android-Art-Study/app/src/main/java/com/changxin/android_art_study/Chapter1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.changxin.android_art_study.Chapter1;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,10 +13,13 @@ import android.widget.Button;
 
 import com.changxin.android_art_study.R;
 
+import java.util.List;
+
 public class MainActivity extends Activity {
     private String TAG = this.getClass().getSimpleName();
     private Button mButtonSkip;
     private Intent mIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +33,22 @@ public class MainActivity extends Activity {
         mButtonSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mIntent = new Intent(MainActivity.this, SecondActivity.class);
-                mIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                //mIntent = new Intent(MainActivity.this, SecondActivity.class);
+                mIntent = new Intent();
+                //mIntent.setAction("com.changxin.art.study.a");
+                // mIntent.addCategory("com.changxin.art.study.c");
+                mIntent.setData(Uri.parse("http://abc"));
+                PackageManager pm = getPackageManager();
+                //判断，如果这个值 为null，说明上面的匹配信息没有成功
+                ComponentName componentName = mIntent.resolveActivity(pm);
+
+                //这个方法用来获取所有符合匹配信息的Acitivity
+                List<ResolveInfo> resolveInfos = pm.queryIntentActivities(mIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                Log.d(TAG, "resolveInfos: " + resolveInfos.size());
+
+                for (ResolveInfo re : resolveInfos) {
+                    Log.d(TAG, "onClick: " + re.activityInfo);
+                }
                 /**
                  * Activity的启动过程：Activity---->startActivity(Intent intent)----->startActivity(Intent intent, @Nullable Bundle options)
                  * ------------->startActivityForResult(@RequiresPermission Intent intent, int requestCode,
@@ -45,7 +66,9 @@ public class MainActivity extends Activity {
                 /**
                  * 如果一个Acitivity  A启动另一个Acitivity B 那么B会被压到A的任务栈中
                  */
-                startActivity(mIntent);
+                if (componentName != null) {
+                    startActivity(mIntent);
+                }
             }
         });
     }
@@ -60,7 +83,7 @@ public class MainActivity extends Activity {
     }
 
     /**
-     *onResume()和onPause()是从Activity是否在前台来回调的，除了这两个，其他也没什么区别
+     * onResume()和onPause()是从Activity是否在前台来回调的，除了这两个，其他也没什么区别
      */
     @Override
     protected void onResume() {
@@ -69,8 +92,8 @@ public class MainActivity extends Activity {
     }
 
     /**
-     *当A跳转到B的时候A会先pause，不能做大数据的缓存操作，因为后面的Activity还要启动
-     *
+     * 当A跳转到B的时候A会先pause，不能做大数据的缓存操作，因为后面的Activity还要启动
+     * <p>
      * 在AcitivtyStack中的resumeTopActivityInnerLocked（）方法中有这么一句注释
      * // We need to start pausing the current activity so the top one can be resumed...
      * 说明会先puase
@@ -83,7 +106,7 @@ public class MainActivity extends Activity {
 
     /**
      * 主席讲Actiivity换成透明主题的时候这个方法并不会调用，但是自己试了一下，可以用
-     *
+     * <p>
      * 不能做大数据的存储
      */
     @Override
@@ -106,7 +129,8 @@ public class MainActivity extends Activity {
 
     /**
      * 在onPause()之前或者之后调用，两者并没有绝对的关系
-     *  当Acitivity的配置变化的时候就会被重建，这里可以通过configChanges来配置消除一些配置变化时Acitivty的重建
+     * 当Acitivity的配置变化的时候就会被重建，这里可以通过configChanges来配置消除一些配置变化时Acitivty的重建
+     *
      * @param outState
      */
     @Override
@@ -117,12 +141,13 @@ public class MainActivity extends Activity {
 
     /**
      * 在onStart()之前调用
-     *
-     *在onCreate()和onRestoreInstanceState()中都是可以恢复的，但是两者不同的是：
+     * <p>
+     * 在onCreate()和onRestoreInstanceState()中都是可以恢复的，但是两者不同的是：
      * onRestoreInstanceState():当它调用的时候savedInstanceState是一定有值的
      * onCreate()：当用onCreate()恢复的时候savedInstanceState不一定有值，使用之前一定要判断
-     *
+     * <p>
      * 官方文档推荐用onRestoreInstanceState（）
+     *
      * @param savedInstanceState
      */
     @Override
@@ -134,6 +159,7 @@ public class MainActivity extends Activity {
     /**
      * 如果要启动的Acitivity在栈顶，并且设置了singleTop，那么再次启动这个Acitivty的时候，就会回调这个方法，
      * onCreate()和onStart()不会回调。
+     *
      * @param intent
      */
     @Override
