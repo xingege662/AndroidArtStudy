69827b056cb73389f610c771833b97a76a52b402
xingege662 <924717778@qq.com>
2016-12-28 16:56:05 +0800

    49

--- Android-Art-Study/chapter2/src/main/java/com/changxin/chapter2/model/User2.java
+++ Android-Art-Study/chapter2/src/main/java/com/changxin/chapter2/model/User2.java
@@ -12,32 +12,26 @@ public class User2 implements Parcelable {
     private int age;
     private String name;
     private String sex;
+    private Book book;
 
-    public User2(int age, String name, String sex) {
+    public User2(int age, String name, String sex,Book book) {
         this.age = age ;
         this.name = name;
         this.sex = sex;
+        this.book = book;
     }
+
 
     protected User2(Parcel in) {
         age = in.readInt();
         name = in.readString();
         sex = in.readString();
+        book = in.readParcelable(Book.class.getClassLoader());
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(age);
-        dest.writeString(name);
-        dest.writeString(sex);
-    }
-
-    @Override
-    public int describeContents() {
-        //几乎所有情况下都返回的是0，仅当有文件描述时返回1
-        return 0;
-    }
-
+    /**
+     * 反序列化
+     */
     public static final Creator<User2> CREATOR = new Creator<User2>() {
         @Override
         public User2 createFromParcel(Parcel in) {
@@ -49,4 +43,22 @@ public class User2 implements Parcelable {
             return new User2[size];
         }
     };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * 序列化
+     * @param dest
+     * @param flags
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(age);
+        dest.writeString(name);
+        dest.writeString(sex);
+        dest.writeParcelable(book, flags);
+    }
 }
