328c55064e69620f03a9fd4b6c6070b308b5ee37
xingege662 <924717778@qq.com>
2017-01-11 15:56:46 +0800

    177

--- Android-Art-Study/chapter4/src/main/java/com/voole/chapter4/MainActivity.java
+++ Android-Art-Study/chapter4/src/main/java/com/voole/chapter4/MainActivity.java
@@ -1,10 +1,14 @@
 package com.voole.chapter4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    View view;
+    Activity a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
