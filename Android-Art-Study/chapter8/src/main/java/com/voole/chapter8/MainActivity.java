a378aac57da802dfdd5be04f2ef97496ad1fdf07
xingege662 <924717778@qq.com>
2017-01-24 14:19:06 +0800

    302

--- Android-Art-Study/chapter8/src/main/java/com/voole/chapter8/MainActivity.java
+++ Android-Art-Study/chapter8/src/main/java/com/voole/chapter8/MainActivity.java
@@ -1,7 +1,7 @@
 package com.voole.chapter8;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
