3ccd7e881a1d22b6232016219e99f9b6e7c15391
xingege662 <924717778@qq.com>
2017-02-06 17:39:36 +0800

    380

--- Android-Art-Study/chapter10/src/main/java/com/voole/chapter10/MainActivity.java
+++ Android-Art-Study/chapter10/src/main/java/com/voole/chapter10/MainActivity.java
@@ -2,12 +2,29 @@ package com.voole.chapter10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ThreadLocal<Boolean> mThreadLocal = new ThreadLocal<>();
+    private String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mThreadLocal.set(true);
+        Log.d(TAG, "MainThread:"+mThreadLocal.get());
+        new Thread("thread1"){
+            @Override
+            public void run() {
+                mThreadLocal.set(false);
+                Log.d(TAG, "thread1:"+mThreadLocal.get());
+            }
+        }.start();
+        new Thread("thread2"){
+            @Override
+            public void run() {
+                Log.d(TAG, "thread2:"+mThreadLocal.get());
+            }
+        }.start();
     }
 }
