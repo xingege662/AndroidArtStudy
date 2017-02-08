bb03460b708b2ad22134e9802f7c406b999de4c5
xingege662 <924717778@qq.com>
2017-02-08 15:39:18 +0800

    4种加载策略

--- Android-Art-Study/chapter12/src/main/java/com/voole/chapter12/loader/ImageResizer.java
+++ Android-Art-Study/chapter12/src/main/java/com/voole/chapter12/loader/ImageResizer.java
@@ -5,12 +5,23 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import java.io.InputStream;
+
 /**
  * Created by changxin on 2017/2/8.
  */
 
 public class ImageResizer {
     public static String TAG ="ImageResizer";
+
+    /**
+     * 从resource下加载Bitmap
+     * @param resources
+     * @param resId
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
     public static Bitmap decodeBitmapFromResource(Resources resources, int resId, int reqWidth, int reqHeight){
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
@@ -23,6 +34,63 @@ public class ImageResizer {
 
     }
 
+    /**
+     *  从file文件中加载Bitmap
+     * @param filePath
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static Bitmap decodeBitmapFromFile(String filePath,int reqWidth ,int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath,options);
+
+        options.inSampleSize = calculateInsamplesize(options,reqWidth,reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath,options);
+    }
+
+    /**
+     *  从流中加载Bitmap
+     * @param in
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public  static Bitmap decodeBitmapFromStream(InputStream in, int reqWidth,int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(in, null, options);
+        options.inSampleSize = calculateInsamplesize(options,reqWidth,reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeStream(in,null,options);
+    }
+
+    /**
+     * 从字节中加载Bitmap
+     * @param data
+     * @param offset
+     * @param length
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static Bitmap decodeBitmapFromByteArray(byte[] data,int offset,int length,int reqWidth,int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(data,offset,length,options);
+        options.inSampleSize = calculateInsamplesize(options,reqWidth,reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(data,offset,length,options);
+    }
+    /**
+     * 计算inSampleSize
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
     private static int calculateInsamplesize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         if (reqWidth == 0 || reqHeight == 0) {
             return 1;
